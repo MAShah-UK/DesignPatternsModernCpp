6ce24d871a253fc3355449a2d185c6d1ddbb4246
MAShah-UK <m.a.shah@live.co.uk>
2018-09-18 21:15:44 +0100

    Covered monostate design pattern, white box unit/integration testing, black box acceptance/system testing. Completed singleton challenge, and section.

--- Sections/Singleton/main.cpp
+++ Sections/Singleton/main.cpp
@@ -5,7 +5,12 @@
 #include <vector>
 #include <gtest/gtest.h>
 
-class SingletonDatabase {
+class Database {
+public:
+    virtual int get_population(const std::string &name) const = 0;
+};
+
+class SingletonDatabase : Database {
     std::map<std::string, int> capitals;
 
     // Make constructor private so that only members can instantiate this class.
@@ -30,16 +35,30 @@ public:
         return db; // Always returns the same instance.
     }
 
-    int get_population(const std::string &name) {
-        return capitals[name];
+    int get_population(const std::string &name) const override {
+        return capitals.at(name);
     }
-public:
-
 };
 
+class DummyDatabase : public Database {
+    std::map<std::string, int> capitals;
+public:
+    DummyDatabase() {
+        capitals["alpha"] = 1;
+        capitals["beta"] = 2;
+        capitals["gamma"] = 3;
+    }
+    int get_population(const std::string &name) const override {
+        return capitals.at(name);
+    }
+};
+
+// Bad code: tightly coupled with SingletonDatabase and can't easily be
+// tested with a custom data set. The database being accessed might
+// change at anytime which will make this test fail.
 class SingletonRecordFinder {
 public:
-    int total_population(std::vector<std::string> names) {
+    int total_population(const std::vector<std::string> &names) {
         auto &db = SingletonDatabase::get();
         int count {};
         for(const auto &city: names) {
@@ -49,11 +68,37 @@ public:
     }
 };
 
+// Good code: supports dependency injection which makes it easier to test.
+class ConfigurableRecordFinder {
+    Database &db;
+public:
+    ConfigurableRecordFinder(Database &db)
+        : db{db} {}
+
+    int total_population(const std::vector<std::string> &names) {
+        int count {};
+        for(const auto &city: names) {
+            count += db.get_population(city);
+        }
+        return count;
+    }
+};
+
+// This is an integration test because it's testing multiple facets
+// of the program at once, i.e. SingletonRecordFinder with a live
+// database.
 TEST(RecordFinderTests, SingletonTotalPopulationTest) {
     SingletonRecordFinder rf;
     std::vector<std::string> names{"Seoul", "Mexico City"};
     int total = rf.total_population(names);
     EXPECT_EQ(17500000+17400000, total);
+}
+
+// This is a unit test because it's testing a small section of the code.
+TEST(RecordFinderTests, DependantTotalPopulationTest) {
+    DummyDatabase db;
+    ConfigurableRecordFinder rf{db};
+    EXPECT_EQ(3, rf.total_population({"alpha", "beta"}));
 }
 
 int main(int argc, char* argv[]) {
@@ -67,3 +112,15 @@ int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+/*
+Challenge: Is factory returning singletons?
+struct SingletonTester {
+    template <typename T>
+    bool is_singleton(function<T*()> factory) {
+        T* inst1 = factory();
+        T* inst2 = factory();
+        return inst1 == inst2;
+    }
+};
+ */
