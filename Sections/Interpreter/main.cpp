8ef1c030d437d70460efb0e77b90c50748cc994d
MAShah-UK <m.a.shah@live.co.uk>
2018-10-07 21:54:29 +0100

    Corrected bugs in lexer and parser.

--- Sections/Interpreter/main.cpp
+++ Sections/Interpreter/main.cpp
@@ -46,112 +46,105 @@ public:
     }
 };
 
-class ExpressionInterpreter {
-    std::string exp;
-    std::vector<Token> tokens;
-
-public:
-    ExpressionInterpreter(const std::string& exp)
-            : exp{exp} {
-        lex();
-    }
-
-    std::string get_tokens() const {
-        std::ostringstream result;
-        for(const Token& token : tokens) {
-            result << token << ' ';
-        }
-        return result.str();
-    }
-
-    void lex() { // Lexing/tokenisation converts a string into a token.
-        std::vector<Token> result;
-        for(size_t i{}; i < exp.size(); ++i) {
-            switch(exp[i]) {
-                case '+': {
-                    result.emplace_back(Token::plus, "+");
-                }
-                case '-': {
-                    result.emplace_back(Token::minus, "-");
-                    break;
-                }
-                case '(': {
-                    result.emplace_back(Token::lparen, "(");
-                    break;
-                }
-                case ')': {
-                    result.emplace_back(Token::rparen, ")");
-                    break;
-                }
-                default: {
-                    std::ostringstream buffer;
-                    buffer << exp[i];
-                    for(i += 1; i < exp.size(); ++i) {
-                        if(std::isdigit(exp[i])) {
-                            buffer << exp[i];
-                        } else {
-                            result.emplace_back(Token::integer, buffer.str());
-                            i -= 1;
-                            break;
-                        }
-                    }
-                }
+std::vector<Token> lex(std::string exp) { // Lexing/tokenisation converts a string into a token.
+    std::vector<Token> result;
+    for(size_t i{}; i < exp.size(); ++i) {
+        switch(exp[i]) {
+            case '+': {
+                result.emplace_back(Token::plus, "+");
+                break;
             }
-        }
-        tokens = std::move(result);
-    }
-    void parse() {
-        auto result = std::make_unique<BinaryOperation>();
-        bool have_lhs{false};
-        for(size_t i{}; i < tokens.size(); ++i) {
-            auto& token {tokens[i]};
-            switch(token) {
-                case Token::integer: {
-                    int to_int = std::stoi(token.text);
-                    auto to_integer = std::make_shared<Integer>(to_int);
-                    if(!have_lhs) {
-                        result->lhs = to_integer;
-                        have_lhs = true;
-                    }
-                    break;
-                }
-                case Token::plus: {
-                    result->type = BinaryOperation::addition;
-                    break;
-                }
-                case Token::minus: {
-                    result->type = BinaryOperation::subtraction;
-                    break;
-                }
-                case Token::lparen: {
-                    size_t j = i;
-                    for(; j < tokens.size(); ++j) {
-                        if(tokens[j].type == Token::rparen) {
-                            break;
-                        }
-                    }
-                    std::vector<Token> subexpression(&tokens[i+1], &tokens[j]);
-                    auto element = parse(subexpression);
-                    if(!have_lhs) {
-                        result->lhs = element;
-                        have_lhs = true;
+            case '-': {
+                result.emplace_back(Token::minus, "-");
+                break;
+            }
+            case '(': {
+                result.emplace_back(Token::lparen, "(");
+                break;
+            }
+            case ')': {
+                result.emplace_back(Token::rparen, ")");
+                break;
+            }
+            default: {
+                std::ostringstream buffer;
+                buffer << exp[i];
+                for(i += 1; i < exp.size(); ++i) {
+                    if(std::isdigit(exp[i])) {
+                        buffer << exp[i];
                     } else {
-                        result->rhs = element;
+                        result.emplace_back(Token::integer, buffer.str());
+                        i -= 1;
+                        break;
                     }
-                    i = j;
-                    break;
                 }
             }
         }
     }
-};
+    return result;
+}
+std::unique_ptr<BinaryOperation> parse(std::vector<Token> tokens) {
+    auto result = std::make_unique<BinaryOperation>();
+    bool have_lhs{false};
+    for(size_t i{}; i < tokens.size(); ++i) {
+        auto& token {tokens[i]};
+        switch(token.type) {
+            case Token::integer: {
+                int to_int = std::stoi(token.text);
+                auto to_integer = std::make_unique<Integer>(to_int);
+                if(!have_lhs) {
+                    result->lhs = std::move(to_integer);
+                    have_lhs = true;
+                } else {
+                    result->rhs = std::move(to_integer);
+                }
+                break;
+            }
+            case Token::plus: {
+                result->type = BinaryOperation::addition;
+                break;
+            }
+            case Token::minus: {
+                result->type = BinaryOperation::subtraction;
+                break;
+            }
+            case Token::lparen: {
+                size_t j = i;
+                for(; j < tokens.size(); ++j) {
+                    if(tokens[j].type == Token::rparen) {
+                        break;
+                    }
+                }
+                std::vector<Token> subexpression(&tokens[i+1], &tokens[j]);
+                auto element = parse(subexpression);
+                if(!have_lhs) {
+                    result->lhs = std::move(element);
+                    have_lhs = true;
+                } else {
+                    result->rhs = std::move(element);
+                }
+                i = j;
+                break;
+            }
+        }
+    }
+    return result;
+}
 
 int main() {
-    ExpressionInterpreter exp{"(13-4)-(12+1)"};
-    std::cout << exp.get_tokens() << std::endl;
-    try {
-        exp.parse();
-        std::cout
+    std::string expression = "(13-4)-(12+1)";
+    auto tokens = lex(expression);
+    for(const auto& token : tokens) {
+        std::cout << '\'' << token.text << '\'' << ' ';
     }
+    std::cout << std::endl;
+
+    try {
+        auto parsed = parse(tokens);
+        std::cout << expression << " = " << parsed->eval() << std::endl;
+    } catch(const std::exception& e) {
+
+    }
+
     return 0;
 }
